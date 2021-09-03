2ccecff10302f130e40d8fe2eacf8f6284fc5c6d
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-09-02 22:39:56 -0600

    sets

---
+++ 01-introduccion/06-sets.dart
@@ -0,0 +1,10 @@
+main(){
+	// Sets
+	Set<String> paises = {'Guatemala', 'El Salvador', 'Honduras', 'Mexico'};
+	paises.add('Pànama');
+	paises.add('Pànama');
+	paises.add('Pànama');
+	paises.add('Pànama');
+
+	print(paises);
+}
