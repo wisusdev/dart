4fd7ff731383355dd4adae9e54dc5bea08fc33b1
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-09-02 22:26:22 -0600

    numeros

--- 01-introduccion/02-numeros.dart
+++ 01-introduccion/02-numeros.dart
@@ -1,3 +1,14 @@
 main(){
-	
+	// Nùmeros
+	int a = 10;
+	double b = 7.75;
+
+	int? c = null; // usamos "?" para indicar que un valor puede ser null
+
+	int _d = 9;
+	double $e = 21;
+
+	double f = _d * $e;
+
+	print(f);
 }
