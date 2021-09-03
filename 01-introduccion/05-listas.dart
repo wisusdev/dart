842e1c73a7deb39e4bda2ab9ec4ce3e35a508baa
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-09-02 22:39:49 -0600

    Convertir lista a set

--- 01-introduccion/05-listas.dart
+++ 01-introduccion/05-listas.dart
@@ -2,6 +2,14 @@ main(){
 	// Listas
 	List<String> misColores = ['Rojo', 'Verde', 'Negro', 'Blanco'];
 	misColores[1] = 'Amarillo';
+  misColores.add('Azul');
+  misColores.add('Azul');
+  misColores.add('Azul');
+  misColores.add('Azul');
 
 	print(misColores);
+
+  Set<String> coloresSet = misColores.toSet();
+
+  print(coloresSet.toList());
 }
