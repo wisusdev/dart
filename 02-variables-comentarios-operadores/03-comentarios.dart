96314054658a98ebed0202d35e63ad63faec3675
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-09-05 15:52:26 -0600

    comentarios

---
+++ 02-variables-comentarios-operadores/03-comentarios.dart
@@ -0,0 +1,22 @@
+main(){
+	// Este es un comentario
+	int a = 10;
+
+  final colores = [
+    // Primer color
+    'Color 01' // [0]
+
+    // Segundo color
+    'Color 02' // [1]
+
+    // Tercer color
+    'Color 03' // [2]
+  ];
+
+  /*
+    Este es un comentario en bloque
+  */
+
+  /// Comentario especial donde se puede usar para hacer referencia a  [datos]
+}
+
