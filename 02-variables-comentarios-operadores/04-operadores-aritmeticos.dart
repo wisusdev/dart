9ff107597735b40950f65a2547645d2dad7fbf0b
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-09-05 15:52:40 -0600

    operadores aritmeticos

---
+++ 02-variables-comentarios-operadores/04-operadores-aritmeticos.dart
@@ -0,0 +1,18 @@
+main(){
+	// Operadores aritmticos
+	int num_ini = 35;
+
+	print(num_ini + 2); // Suma	(+) 42
+	print(num_ini - 2); // Resta (-) 42
+	print(num_ini * 2); // Multiplicación (*) 42
+	print(num_ini / 2); // División	(/) 42.0
+	print(num_ini ~/ 2);	// Divide y retorna un resultado entero	~/ 		
+	
+
+	double value = 42.0;
+	value += 1; print(value); // 43.0
+	value -= 1; print(value); // 42.0
+	value *= 2; print(value); // 84.0
+	value /= 2; print(value); // 42.0
+
+}
