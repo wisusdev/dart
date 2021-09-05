340d3169ac15c074c474dda993740910945f66ab
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-09-05 15:53:05 -0600

    operadores de incremento y decremento

---
+++ 02-variables-comentarios-operadores/06-ope-incremento-y-decremento.dart
@@ -0,0 +1,10 @@
+main(){
+
+  int num_ini = 35;
+
+  ++num_ini;	// Incrementa	++	36	 	 	 	 
+ 	num_ini++; // Incrementa	++	36	 	 	 	 
+ 	--num_ini; // Decrementa	--	34	 	 	 	 
+ 	num_ini--; // Decrementa	--	34	 	 	 	 
+ 	num_ini%3; // Valor restante	%	2
+}
