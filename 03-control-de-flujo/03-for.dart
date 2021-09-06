8670c0318302dc8a960ec80fe21226875a4a368c
Jesus Avelar <jesus.avelar.nav@gmail.com>
2021-09-06 12:30:08 -0600

    for

---
+++ 03-control-de-flujo/03-for.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  int count = 10;
+  for (int i = 0; i < count; i++) {
+    print('Cilco ${2 + 2}');
+  }
+
+  print("\n");
+
+  int multi = int.parse(stdin.readLineSync() ?? '1');
+  for (int i = 1; i <= 10; i++) {
+    print("$multi * $i = ${multi * i}");
+  }
+}
